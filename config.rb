66980d6439cc90996f6949479e52880052dc709d
Dom Smith <dom@teneightfive.com>
2015-01-29 17:08:50 +0000

    Give travis access to deploy via secure token

--- config.rb
+++ config.rb
@@ -138,12 +138,15 @@ configure :build do
 end
 
 
-activate :deploy do |deploy|
-  deploy.method = :git
-  deploy.build_before = true
-  # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  # deploy.branch   = 'custom-branch' # default: gh-pages
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+if ENV['TRAVIS']
+  activate :deploy do |deploy|
+    deploy.method = :git
+    deploy.build_before = true
+    deploy.remote   = "https://#{ENV['GH_TOKEN']}:@github.com/#{ENV['TRAVIS_REPO_SLUG']}.git"
+  end
+else
+  activate :deploy do |deploy|
+    deploy.method = :git
+    deploy.build_before = true
+  end
 end
